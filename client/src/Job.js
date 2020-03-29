b757faf58e5da79ca2a1d2f1893eecf1e408e28a
a8hok <ashokkumar.explore@gmail.com>
2020-03-29 22:37:58 +0530

    removed mock values, fetch values from backend server through api, UI alignment

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,15 +1,21 @@
-import React, {
-    Fragment
-} from 'react';
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
 
-const Job = ({mockJob}) => {
+const Job = ({job}) => {
     return (
-        <Fragment>
-            <div className="single-job">
-                {mockJob.title} - 
-                {mockJob.company}
+        <Paper className='job'>
+            <div className="flex-align-mid">
+                <div className="job-title-location">
+                    <Typography variant='h6'>{job.title}</Typography>
+                    <Typography variant='h5'>{job.company}</Typography>
+                    <Typography>{job.location}</Typography>
+                </div>
             </div>
-        </Fragment>
+            <div className="flex-align-mid">
+                <Typography>{job.created_at.split(' ').slice(0,3).join(' ')}</Typography>
+            </div>
+        </Paper>
     )
 }
 
--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,33 +1,33 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import { Typography } from '@material-ui/core';
-
 import Job from './Job';
+
+const JOB_API_URL = 'http://localhost:3001/jobs';
+
+// Fetch jobs from server
+const fetchJobs = async (setJobCB) => {
+    const response = await fetch(JOB_API_URL);
+    const jobs = await response.json();
+    setJobCB(jobs);
+}
 
 const Jobs = () => {
     const [mockJobList, setJobList] = useState([]);
 
-    const setMockJob = () => {
-        setJobList(
-            [
-                { title: 'Sr.Software Engineer', 'company': 'Facebook'},
-                { title: 'Software Engineer', 'company': 'Google'},
-                { title: 'Ass.Software Engineer', 'company': 'Youtube'},
-            ]
-        )
-    }
-
     useEffect(() => {
-        setMockJob();
-    });
+        fetchJobs(setJobList);
+    }, []);
 
     return (
         <Fragment>
-            <Typography variant="h1">
+            <Typography variant="h4" component="h1"> 
                 Entry level software Job list
             </Typography>
-            {
-                mockJobList.map(job => <Job mockJob={job} />)
-            }
+            <div className="job-list">
+                {
+                    mockJobList.map(job => <Job job={job} />)
+                }
+            </div>
         </Fragment>
     )
 }
