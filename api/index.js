0473112ce947562c0c751aee59f6075eaf8a4b63
a8hok <ashokkumar.explore@gmail.com>
2020-03-29 22:37:24 +0530

    added cors to avoid cross origin issues

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 
 const { _getKeyValueRedis } = require('../worker/store/redis');
 
 const app = express();
+app.use(cors());
 
 // Get jobs from Redis
-app.get('/', async (req, res) => {
+app.get('/jobs', async (req, res) => {
     const jobs = await _getKeyValueRedis();
     return res.send(jobs);
 });
 
-app.listen('3001', () => (
-    console.log('server listening at port 3001')
-));
+app.listen('3001', () => console.log('server listening at port 3001'));
