dbea8de05f33e3e5d16ac9261dcb6a902b2b7759
a8hok <ashokkumar.explore@gmail.com>
2020-03-29 19:52:27 +0530

    Filtered junior jobs in result set, set the values into redis

--- worker/index.js
+++ worker/index.js
@@ -1,12 +1,14 @@
 const CronJob = require('cron').CronJob;
+
 const fetchGithubJobs = require('./tasks/fetch-github');
 
+// Runs every minute.
 const job = new CronJob(
-	'* * * * *',
-	fetchGithubJobs,
-	null,
-	true,
-	'America/Los_Angeles'
+    '* * * * *',
+    fetchGithubJobs,
+    null,
+    true,
+    'America/Los_Angeles'
 );
 
 job.start();
--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,18 +1,49 @@
 const fetch = require('node-fetch');
+const redis = require('redis');
+const { promisify } = require("util");
 
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+// Set values into Redis
+const _setKeyValueRedis = async (juniorJobs) => {
+    const setJobsRedis = await setAsync('github', JSON.stringify(juniorJobs));
+    console.log(juniorJobs.length);
+}
+
+// Filter Junior jobs only
+const _filterJuniorJobs = (allJobs) => {
+    return allJobs.filter((job) => {
+        const jobTitle = job.title.toLowerCase();
+        if (
+            jobTitle.includes('senior') ||
+            jobTitle.includes('manager') ||
+            jobTitle.includes('sr.') ||
+            jobTitle.includes('architect')
+        ) {
+            return false;
+        }
+        return true;
+    });
+}
+
+// Fetch jobs from github api
 const fetchGithubJobs = async () => {
-	let resultCount = 1,
-		page = 0;
-	const resultSet = [];
-	const baseUrl = 'https://jobs.github.com/positions.json';
-	while (resultCount > 0) {
-		const response = await fetch(`${baseUrl}?page=${page}`);
-		const jsonRes = await response.json();
-		resultSet.push(...jsonRes);
-		resultCount = jsonRes.length;
-		page++;
-		console.log(jsonRes.length);
-	}
+    let resultCount = 1;
+    let page = 0;
+    const allJobs = [];
+    const baseUrl = 'https://jobs.github.com/positions.json';
+    while (resultCount > 0) {
+        const response = await fetch(`${baseUrl}?page=${page}`);
+        const jsonRes = await response.json();
+        allJobs.push(...jsonRes);
+        resultCount = jsonRes.length;
+        page++;
+        console.log(jsonRes.length);
+    }
+    const juniorJobs = _filterJuniorJobs(allJobs);
+    _setKeyValueRedis(juniorJobs);
 };
 
 module.exports = fetchGithubJobs;
